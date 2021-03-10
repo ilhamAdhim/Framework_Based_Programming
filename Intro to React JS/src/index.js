cac3c3838d79f0832e7ec6696f6a99a4be0b2a9f
ilhamadhim <ilhamm179@gmail.com>
2021-03-10 10:48:20 +0700

    Call login component week3

--- Intro to React JS/src/index.js
+++ Intro to React JS/src/index.js
@@ -13,7 +13,7 @@ class StateFullComponent extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Login />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
