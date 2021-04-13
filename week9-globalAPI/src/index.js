95291688a52f9a53ac19ca64c93765fba05fd1c5
ilhamadhim <ilhamm179@gmail.com>
2021-04-13 22:44:54 +0700

    Assignment week 9 done

---
+++ week9-globalAPI/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import BlogPost from "./components/BlogPost";
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <BlogPost />,
+  document.getElementById('content')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
